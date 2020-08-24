05592fb53dbada3302e0239e36bcdfb39b25c7ba
Arvie Delgado <11804514+arviedelgado@users.noreply.github.com>
2020-08-24 19:29:14 +0800

    Working code

---
+++ HostApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace HostApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var content = ReadInput();
+                WriteOutput(DateTime.Now.Ticks + " echo: " + content);
+            }
+        }
+
+        static string ReadInput()
+        {
+            var stdin = Console.OpenStandardInput();
+            var bytes = new byte[4];
+            stdin.Read(bytes, 0, 4);
+            var length = BitConverter.ToInt32(bytes, 0);
+            var message = "";
+            for (var i = 0; i < length; i++)
+            {
+                message += (char)stdin.ReadByte();
+            }
+            var content = JsonDocument.Parse(message).RootElement
+                            .GetProperty("content").GetString();
+            return content;
+        }
+
+        static void WriteOutput(string content)
+        {
+            using var stream = new MemoryStream();
+            using var writer = new Utf8JsonWriter(stream);
+            writer.WriteStartObject();
+            writer.WriteString("content", content);
+            writer.WriteEndObject();
+            writer.Flush();
+            stream.Seek(0, SeekOrigin.Begin);
+            var bytes = stream.ToArray();
+            var length = bytes.Length;
+            var stdout = Console.OpenStandardOutput();
+            stdout.WriteByte((byte)((length >> 0) & 0xFF));
+            stdout.WriteByte((byte)((length >> 8) & 0xFF));
+            stdout.WriteByte((byte)((length >> 16) & 0xFF));
+            stdout.WriteByte((byte)((length >> 24) & 0xFF));
+            stdout.Write(bytes, 0, bytes.Length);
+            stdout.Flush();
+        }
+    }
+}
---
+++ HostApp/app/background.js
@@ -0,0 +1,37 @@
+﻿var port = null;
+
+function appendMessage(text) {
+	console.log(text);
+}
+
+function sendNativeMessage(content = new Date().toISOString()) {
+	if (port == null)
+		connect();
+	var message = {
+		"content": content
+	}
+	port.postMessage(message);
+	appendMessage("Sent message: " + JSON.stringify(message));
+	return true;
+}
+
+function onNativeMessage(message) {
+	appendMessage("Received message: " + JSON.stringify(message));
+}
+
+function onDisconnected() {
+	appendMessage("Failed to connect: " + chrome.runtime.lastError.message);
+	port = null;
+}
+
+function connect() {
+	var hostName = "com.my_company.my_application";
+	if (port != null) {
+		appendMessage("Already connected to native messaging host " + hostName);
+		return;
+	}
+	appendMessage("Connecting to native messaging host " + hostName);
+	port = chrome.runtime.connectNative(hostName);
+	port.onMessage.addListener(onNativeMessage);
+	port.onDisconnect.addListener(onDisconnected);
+}
